f96ecb106373c647c0c88e275c89a3832853e83b
sam-pettyjohn <sammpj47@gmail.com>
2022-04-19 18:57:27 -0500

    app.js created

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+  
+};
+
+printProfileData(profileDataArgs);
